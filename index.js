ae1a31e2df80593c58e379adbf34f40691f08a7b
swapnil <pawarswapnil0579@gmail.com>
2022-11-26 13:37:16 +0530

    Updates

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDom from "react-dom";
+import './index.css';
+import About from './About.jsx';
+
+var data='React';
+
+ReactDom.render(
+   <>
+		<About></About>
+		
+   </>,document.getElementById('root')
+);
+
+
+
+
+
+
+
